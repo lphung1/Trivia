3a8a20e8daa6b0fa783407484b29d796c98dea2d
lphung1 <loi_phung81@yahoo.com>
2018-11-27 17:42:16 -0500

    added Question Activity layout

---
+++ app/src/main/java/com/example/loi/trivia/QuestionActivity.java
@@ -0,0 +1,32 @@
+package com.example.loi.trivia;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
+
+
+    }
+
+
+}
